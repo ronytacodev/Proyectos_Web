8fae9f483a33e71b291a171d9b8fe5288ea7786c
RonyTcdev <rtcardenas26@gmail.com>
2022-10-24 01:58:35 -0500

    avance de codigo js del slider

---
+++ proyectos/pagina_web_u2/js/slider.js
@@ -0,0 +1,7 @@
+(function() {
+
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelectorAll('#next');
+    const buttonBefore = document.querySelectorAll('#before');
+    
+})();
